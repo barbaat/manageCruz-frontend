09f9f476da78f12b73794916a02040967bd694d9
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-11-12 18:29:37 +0100

    Refactor AlbaranList component to include client filtering

--- src/pages/albaran/AlbaranList.jsx
+++ src/pages/albaran/AlbaranList.jsx
@@ -1,39 +1,82 @@
-import { Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Row, Col, Form } from 'react-bootstrap';
+import Select from 'react-select';
 import { useListAnimation } from '../../services/utils/animationsList.js';
-import { useEffect, useState } from 'react';
 import albaranService from '../../services/api/albaran.js';
 import AlbaranCard from '../../components/AlbaranCard.jsx';
 import CustomNavbar from '../../components/NavbarComponent.jsx';
+import userService from '../../services/api/users.js';
 
 export default function AlbaranList() {
-
     const [albaranes, setAlbaranes] = useState([]);
+    const [clientes, setClientes] = useState([]);
+    const [selectedCliente, setSelectedCliente] = useState(null);
+    const [albaranesShow, setAlbaranesShow] = useState([]);
     const [parent,] = useListAnimation();
 
     useEffect(() => {
-        const getAlbaranes = async () => {
-            const result = await albaranService.getAllAlbaran();
-            setAlbaranes(result);
-            console.log(result);
-        }
-        getAlbaranes();
+        const fetchData = async () => {
+            const albaranesResult = await albaranService.getAllAlbaran();
+            const clientesResult = await userService.usersByRol("CLIENTE");
+            console.log(clientesResult);
+            setAlbaranes(albaranesResult);
+            setAlbaranesShow(albaranesResult);
+            setClientes(clientesResult);
+        };
+
+        fetchData();
     }, []);
+
+    const handleClienteChange = (selectedOption) => {
+        if (selectedOption.value == null) {
+            setAlbaranesShow(albaranes);
+            setSelectedCliente({ value: null, label: 'Todos los clientes' });
+            return;
+        }
+        setSelectedCliente(selectedOption);
+        const clienteId = selectedOption.value;
+        const filteredAlbaranes = albaranes.filter(albaran => albaran.cliente.id == clienteId);
+        setAlbaranesShow(filteredAlbaranes);
+    };
+
+    const options = clientes.map(cliente => ({ value: cliente.id, label: `${cliente.name} ${cliente.lastName}` }));
+
+    const customStyles = {
+        option: (provided,) => ({
+            ...provided,
+            color: 'black',
+        }),
+        singleValue: (provided,) => ({
+            ...provided,
+            color: 'black',
+        }),
+    };
 
     return (
         <>
             <CustomNavbar />
             <div className='text-center container'>
                 <h1 className='pb-4 mt-5'><b>Lista de albaranes</b></h1>
-                {albaranes.length === 0 && <p>No hay albaranes</p>}
+                <Form.Group className='pb-4 text-center justify-content-center align-center-items' controlId="formCliente">
+                    <Form.Label>Filtra por cliente:</Form.Label>
+                    <Select
+                        options={[{ value: null, label: 'Todos los clientes' }, ...options]}
+                        onChange={handleClienteChange}
+                        value={selectedCliente}
+                        className="col-6 mx-auto"
+                        styles={customStyles}
+                    />
+                </Form.Group>
+
+                {albaranesShow.length === 0 && <p>No hay albaranes</p>}
                 <Row ref={parent}>
-                    {albaranes.map((albaran) => (
+                    {albaranesShow.map((albaran) => (
                         <Col key={albaran.id} xs={12} md={4}>
                             <AlbaranCard object={albaran} />
                         </Col>
                     ))}
                 </Row>
             </div>
-
         </>
     );
 }
